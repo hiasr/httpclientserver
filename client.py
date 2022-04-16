c7d72df337e0637692d62f3c0449233258dc2294
Ruben Hias <ruben.hias@gmail.com>
2022-04-16 12:41:06 +0200

    Update

--- client.py
+++ client.py
@@ -2,11 +2,208 @@ import socket
 import argparse
 import os
 from bs4 import BeautifulSoup
+import logging
+import mimetypes
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class Request:
+    def __init__(self, command, URI, port, save_path=None):
+        """Adding arguments as class variables
+
+        Args:
+            args (Namespace): Input arguments 
+        """
+        self.type = command
+
+        self.uri, self.target = self.parse_uri(URI)
+        self.port = port
+        self.save_path = save_path
+    
+    def parse_uri(self, URI):
+        if URI[:7] != "http://":
+            raise Exception("URI must be in the format 'http://example.com'!")
+        URI = URI[7:]
+        URI_end = URI.find("/")
+        if URI_end == -1:
+            uri = URI
+            target = "/"
+        else:
+            uri = URI[:URI_end]
+            target = URI[URI_end:]
+        return uri, target
+
+        
+
+    def send(self):
+        """Send the HTTP Request
+        """
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            logger.debug("Created socket")
+            self.sock = s
+
+            s.connect((self.uri, self.port))
+            logger.debug(f"Connected to {self.uri} on port {self.port}!")
+
+            request = f"{self.type} {self.target} HTTP/1.1\r\nHost: {self.uri}\r\n\r\n"
+
+            if self.type in ["POST", "PUT"]:
+                user_input = input("Enter string to send in request: ")
+                request += user_input + "\r\n"
+
+            logger.debug("Sending request...")
+            s.sendall(request.encode())
+            logger.info("Request has been succesfully sent!")
+
+            self.receive()
+
+    def receive(self):
+        """Receive the response from the HTTP Request
+        """
+        s = self.sock
+
+        logger.debug("Receiving headers...")
+        headers = self.receive_headers()
+        logger.debug("Headers succesfully received!")
+
+        if self.type == "HEAD":
+            print(headers)
+            return
+
+        self.headers = self.parse_headers(headers)
+
+        logger.debug("Receiving body...")
+        body = self.receive_body()
+
+        if self.type == "GET":
+            mimetype = self.headers["Content-Type"].split(";")[0]
+
+            encoding="ISO-8859-1"
+            if mimetype == 'text/html':
+                try:
+                    encoding = self.headers["Content-Type"].split(";")[1].split("=")[-1]
+                except:
+                    logger.info("No encoding found")
+
+                body = self.get_images(body, encoding=encoding)
+                with open("body.html", "wb") as file:
+                    file.write(body.encode())
+            else:
+                if self.save_path is None:
+                    print(mimetype)
+                    extension = mimetypes.guess_extension(mimetype)
+                    self.save_path = "body" + extension
+
+                with open(self.save_path, "wb") as file:
+                    file.write(body)
+        else:
+            print(body.decode())
+
+        logger.debug("Body succesfully received!")
+
+
+    def receive_headers(self):
+        """Receive response headers
+
+        Returns:
+            string: response headers and command
+        """
+        # Getting the headers byte per byte while checking for double carriage return,
+        # because then the headers stop and the body begins
+        response = ""
+        while "\r\n\r\n" not in response:
+            response += self.sock.recv(1).decode()
+
+        return response
+    
+    def parse_headers(self, headers): 
+        """Parse the headers in a dictionary for easy access
+
+        Args:
+            headers (string): raw headers and command
+
+        Returns:
+            dict: parsed headers
+        """
+        return dict([line.split(": ") for line in headers.splitlines()[1:-1]])
+
+    def receive_body(self):
+        """Receives the response body
+
+        Returns:
+            bytes: raw body 
+        """
+        # Checking how the content will be sent
+        if "Content-Length" in self.headers:
+            body_length = 0
+            body = bytes()
+            # Keep receiving in blocks of 1024 bytes until the whole message has been received
+            while body_length < int(self.headers["Content-Length"]):
+                block = self.sock.recv(1024)
+                body_length += len(block)
+                body += block
+        else:
+            body = bytes()
+            while True:
+                # Reading the block size as a hexstring byte per byte
+                hex_size = ""
+                while "\r\n" not in hex_size:
+                    hex_size += self.sock.recv(1).decode()
+                    # print(f"Hexstring: {hex_size}")
+
+                # Converting the block size in hex to integer
+                block_size = int(hex_size, 16)
+
+                # If the block size is 0, the whole message is received, RFC 2616 §3.6.1
+                if block_size == 0:
+                    break
+
+                # Read the whole block and write to file
+                while block_size > 0:
+                    block = self.sock.recv(min(1024, block_size))
+                    block_size -= len(block)
+                    body += block
+
+                # Receive 2 bytes to skip carriage return and immediately go to the hexstring for the next block
+                self.sock.recv(2)
+        return body
+
+    def get_images(self, html, encoding):
+        soup = BeautifulSoup(html.decode(encoding=encoding), features="lxml")
+
+        cwd = os.getcwd()
+
+        for img in soup.find_all(["img", "IMG"]):
+
+            img_path = img["src"]
+
+            if "://" not in img_path:
+                port = self.port
+                target = img_path if img_path[0] == "/" else "/" + img_path
+                URI = "http://" + self.uri + target
+            else:
+                URI = img_path
+                port = 80
+
+
+            img_name = img_path[img_path.rfind("/") + 1 :]
+            new_path = cwd + f"/images/{img_name}"
+
+            # TODO: USE THE SAME CONNECTION FOR THIS!!!!!
+            Request("GET", URI, port, save_path=new_path).send()
+            img["src"] = new_path
+
+        return soup.prettify()
 
 
 def parse_arguments():
-    """
-    This function parses all the input arguments and validates them
+    """Parses input arguments and does argument verification
+
+    Returns:
+        Namespace: All the provided arguments in a namespace
     """
     parser = argparse.ArgumentParser(description="Perform HTTP Request")
 
@@ -25,103 +222,11 @@ def parse_arguments():
 
     return parser.parse_args()
 
-def http_head(args):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((args.URI, args.port))
-        s.settimeout(5)
-
-        request = f"HEAD / HTTP/1.1\r\nHost: {args.URI}\r\n\r\n"
-
-        s.sendall(request.encode())
-        response = ""
-        while "\r\n\r\n" not in response:
-            response += s.recv(1).decode()
-        with open('head.html', 'w') as file:
-            file.write(response)
-        
-        
-def http_post(args):
-    
-
-
-def http_get(args, path="/", target="body.html"):
-    # Setting up socket with IPv4 and TCP options
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # Connecting to host and setting timeout
-        s.connect((args.URI, args.port))
-        s.settimeout(5)
-
-        # Send the request
-        request = f"GET /{path} HTTP/1.1\r\nHost: {args.URI}\r\n\r\n"
-        s.sendall(request.encode())
-
-
-        # Getting the headers byte per byte while checking for double carriage return,
-        # because then the headers stop and the body begins
-        response = ""
-        while "\r\n\r\n" not in response:
-            response += s.recv(1).decode()
-
-        # Parsing headers into a dictionary for easy access
-        headers = dict([line.split(": ") for line in response.splitlines()[1:-1]])
-        print(headers)
-
-        # Checking how the content will be sent
-        if "Content-Length" in headers:
-            with open(target, "wb") as file:
-                body_length = 0
-                # Keep receiving in blocks of 1024 bytes until the whole message has been received
-                while body_length < int(headers["Content-Length"]):
-                    block = s.recv(1024)
-                    body_length += len(block)
-                    file.write(block)
-        else:
-            with open(target, "wb") as file:
-                while True:
-                    # Reading the block size as a hexstring byte per byte
-                    hex_size = ""
-                    while "\r\n" not in hex_size:
-                        hex_size += s.recv(1).decode()
-                        # print(f"Hexstring: {hex_size}")
-
-                    # Converting the block size in hex to integer
-                    block_size = int(hex_size, 16)
-
-                    # If the block size is 0, the whole message is received, RFC 2616 §3.6.1
-                    if block_size == 0:
-                        break
-
-                    # Read the whole block and write to file
-                    while block_size > 0:
-                        block = s.recv(min(1024,block_size))
-                        block_size -= len(block)
-                        file.write(block)
-
-                    # Receive 2 bytes to skip carriage return and immediately go to the hexstring for the next block
-                    s.recv(2)
-    
-    get_images(args, target)
-
-
-def get_images(args, path):
-    with open(path, "rb") as file:
-        soup = BeautifulSoup(file, features='lxml')
-    
-    for img in soup.find_all(['img','IMG']):
-        img_path = img['src']
-        img_name = img_path[img_path.rfind('/')+1:]
-        new_path = f"images/{img_name}"
-        http_get(args, img_path, new_path)
-        img['src'] = new_path
-
-    with open(path, "wb") as file:
-        file.write(soup.prettify().encode())
-
+def check_arguments(args):
+    uri = args.URI
 
 
 if __name__ == "__main__":
     args = parse_arguments()
-    if args.command == 'GET':
-        http_get(args)
-    elif args.command == 'HEAD':
-        http_head(args)
+    check_arguments(args)
+    Request(args.command, args.URI, args.port).send()
--- server.py
+++ server.py
@@ -1,94 +1,188 @@
 import threading
 import socket
-from email.utils import formatdate
+from email.utils import formatdate, parsedate
+import time
 import os
+import logging
+import mimetypes
+from http import HTTPStatus
+import traceback
 
-SERVER_HOST = 'localhost'
-SERVER_PORT = 8080
-
-
-
-def generate_headers(path):
-	status_line = "HTTP/1.1 200 OK\r\n"
-	headers = dict()
-	headers['Date'] = formatdate()
-	headers['Content-Length'] = os.path.getsize(path)
-	headers['Content-Type'] = 'text/html'
-
-	header_string = status_line
-	for (key, value) in headers.items():
-		header_string += f"{key}: {value}\r\n"
-	header_string += "\r\n"
-	return header_string
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 
-def send_file(conn, path, file_type):
-	return_headers = generate_headers(path)
-	conn.send(return_headers.encode())
-	with open(path, 'rb') as file:
-		while True:
-			chunk = file.read(1024)
-			if len(chunk) == 0:
-				break
-			conn.send(chunk)
+class Server:
+    def __init__(self, address="localhost", port=8080):
+        self.address = address
+        self.port = port
+        self.socket = self.setup_socket()
 
-def handle_get(conn, command, headers):
-	conn.setblocking(0)
-	path = command[1]
-	if path == '/':
-		send_file(conn, f"www/index.html", 'text/html')
-	elif path == '/image.png':
-		send_file(conn, f"www/{path}", "image/png")
-	else:
-		raise Exception("File not found")
+    def setup_socket(self):
+        """Initializing socket"""
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((self.address, self.port))
+        return s
 
-	
+    def start(self):
+        """Start listening for connections
+        Spawns a new connection handler thread for each incoming connection
+        """
+        logger.debug("Started listening for incoming connections...")
+        self.socket.listen()
 
-def handle_conn(conn, addr):
-	conn.settimeout(2)
-	print("Started connection")
-	data = ""
-	while True: 
-		try:
-			chunk = conn.recv(1024).decode()
-			if not chunk:
-				break
-			data += chunk
-			print(data)
-		except:
-			break
+        while True:
+            conn, addr = self.socket.accept()
+            logger.info(f"Accepted incoming connection from address {addr}!")
 
-	header_end = data.find("\r\n\r\n")
-	command = data[:header_end].splitlines()[0]
-	print(command)
-	headers = dict([line.split(": ") for line in data[:header_end].splitlines()[1:]])
-	data = data[header_end+4:]
-	command = command.split()
+            thr = threading.Thread(target=self.handle_conn, args=(conn,))
+            thr.start()
 
-	if command[0] == 'GET':
-		handle_get(conn, command, headers)
-	elif command[0] == 'HEAD':
-		handle_head(command, headers)
-	elif command[0] == 'PUT':
-		handle_put(command, headers, data)
-	elif command[0] == 'POST':
-		handle_post(command, headers, data)
-	else:
-		raise Exception("Not a valid command")
+    def stop(self):
+        logger.info("Shutting down server...")
+        self.socket.shutdown(socket.SHUT_RDWR)
+        self.socket.close()
+
+    def handle_conn(self, conn):
+        """Handles an incoming request
+
+        Args:
+            conn (socket connection): The connection for the incoming request
+        """
+
+        try:
+            conn.settimeout(2)
+            data = bytes()
+
+            try:
+                while b"\r\n\r\n" not in data:
+                    chunk = conn.recv(1024)
+                    if not chunk:
+                        break
+                    data += chunk
+            except TimeoutError:
+                return
+                logger.warn("Connection timed out!")
+
+            header_end = data.find(b"\r\n\r\n")
+            headers = data[:header_end].decode()
+            request_type, path, _ = headers.splitlines()[0].split()
+            logger.debug(f"Incoming {request_type} request for {path}")
+            print(headers.splitlines()[0])
+
+            if f"Host: {self.address}" not in headers.splitlines()[1]:
+                logger.warning("BAD REQUEST")
+                self.send_status_code(conn, HTTPStatus.BAD_REQUEST)
+                return
+
+            headers = dict([line.split(": ") for line in headers.splitlines()[2:]])
+
+            data = data[header_end + 4 :]
+            if "Content-Length" in headers.keys():
+                content_length = headers["Content-Length"]
+                data_size = len(data)
+                while data_size < content_length:
+                    chunk = conn.recv(1024)
+                    data += chunk
+                    data_size += len(chunk)
+
+            if path == "/":
+                path = "/index.html"
+
+            if request_type == "POST":
+                self.save_data(data, path, append=False)
+            if request_type == "PUT":
+                self.save_data(data, path, append=True)
+
+            self.send_file(conn, "www" + path, headers)
+
+            if "Connection" in headers.keys() and headers["Connection"] == "keep-alive":
+                self.handle_conn(conn)
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+            print("Tis genaaid")
+            self.send_status_code(conn, HTTPStatus.INTERNAL_SERVER_ERROR)
+
+        conn.close()
+        logger.debug("Handler thread finished!")
+
+    def generate_headers(self, path=None, code=HTTPStatus.OK):
+        """Generates headers for the response
+
+        Args:
+                path (string): Path to the file to send with the request
+
+        Returns:
+                string: string containing all headers
+        """
+        status_line = f"HTTP/1.1 {code.value} {code.phrase}\r\n"
+        headers = dict()
+        headers["Date"] = formatdate(timeval=None, localtime=False, usegmt=True)
+        if path is not None:
+            headers["Content-Length"] = os.path.getsize(path)
+            headers["Content-Type"] = mimetypes.guess_type(path)[0]
+
+        header_string = status_line
+        for (key, value) in headers.items():
+            header_string += f"{key}: {value}\r\n"
+        header_string += "\r\n"
+
+        return header_string
+
+    def send_file(self, conn, path, headers):
+        """Send file through open connection
+
+        Args:
+                conn (socket.Connection): The connection you want to send the file through
+                path (string): Path to the file to send
+                file_type (string): The file type
+        """
+        if not os.path.exists(path):
+            logger.debug("Path not found, responding with error code...")
+            self.send_status_code(conn, HTTPStatus.NOT_FOUND)
+            return
+
+        if "If-Modified-Since" in headers.keys():
+            last_mtime = os.path.getmtime(path)
+            if last_mtime < time.mktime(parsedate(headers["If-Modified-Since"])):
+                logger.debug(
+                    "Requested file not modified, responding Not Modified status code..."
+                )
+                self.send_status_code(conn, HTTPStatus.NOT_MODIFIED)
+                return
+
+        return_headers = self.generate_headers(path)
+
+        logger.debug("Sending response...")
+        conn.send(return_headers.encode())
+        with open(path, "rb") as file:
+            while True:
+                chunk = file.read(1024)
+                if len(chunk) == 0:
+                    break
+                conn.send(chunk)
+
+    def save_data(self, data, path, append=False):
+        """Save string to file
+
+        Args:
+            data (string): data to be saved
+            path (string): path to save the data to
+            append (bool, optional): whether to append to file or (over)write file. Defaults to False.
+        """
+        logger.debug("Saving incoming data...")
+        open_type = "ab" if append else "wb"
+        with open("www" + path, open_type) as file:
+            file.write(data)
+
+    def send_status_code(self, conn, status_code):
+        headers = self.generate_headers(code=status_code)
+        print(headers)
+        conn.sendall(headers.encode())
 
 
-
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	print("Setting up socket")
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	s.bind((SERVER_HOST, SERVER_PORT))
-	s.listen()
-	print("Listening for incoming connections...")
-
-	while True:
-		conn, addr = s.accept()
-		print(f"Accepted incoming connection from addres {addr}!")
-
-		thr = threading.Thread(target=handle_conn, args=(conn, addr))
-		thr.start()
+if __name__ == "__main__":
+    Server().start()
